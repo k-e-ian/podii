ed28a62726c887eeb33c890446790c20bd1f85fe
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-03 17:43:28 +0300

    add tests to our functions

--- blogger/test/blogger_test.exs
+++ blogger/test/blogger_test.exs
@@ -5,4 +5,17 @@ defmodule BloggerTest do
   test "greets the world" do
     assert Blogger.hello() == :world
   end
+
+  test "fetches blog successfully" do
+    {:ok, response} = Blogger.fetch_blog()
+    assert response.status_code == 200
+    assert response.request_url == "https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91"
+  end
+
+  test "returns a list of uppercase strings" do
+    input = ["cow", "goat", "sheep"]
+    expected_output = ["COW", "GOAT", "SHEEP"]
+
+    assert Blogger.uppercase_list(input) == expected_output
+  end
 end
